f0bcfe7c3da773ae3e192f665495675e7b76ed9a
Stefano-Bernini <stefanobernini26@gmail.com>
2023-06-29 23:30:53 +0200

    fix endpoint

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc',
+    apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=35',
     pokemonList: [],
     loading: true
 })
